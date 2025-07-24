1ceec060afbb71410b48c818dff3f33243069345
Zuhayr Tariq <zuhayrtariq4@gmail.com>
2025-07-24 21:49:18 +0500

    added zuhayr ai

--- src/lib/ai/embeddings.ts
+++
@@ -1,6 +0,0 @@
-import { Pinecone } from "@pinecone-database/pinecone";
-
-const pc = new Pinecone({
-	// biome-ignore lint: Forbidden non-null assertion.
-	apiKey: process.env.PINECONE_API_KEY!,
-});
